0bb44b21ab9d7a9abf2e485002a11ed0287f991b
Dominik <schdomin@gmail.com>
2013-12-09 19:13:20 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -37,24 +37,34 @@ int main( int argc, char** argv )
     //ds get number of available processes
     MPI_Comm_size( MPI_COMM_WORLD, &iNumberOfTasks );
 
-    //ds user information
-    std::cout << "\n---------------------------- DIFFUSION PSE SETUP ----------------------------" << std::endl;
-    std::cout << "   Diffusion Coefficient: "  << dDiffusionCoefficient << std::endl;
-    std::cout << "           Boundary (2D): [" << prBoundaries.first << ", " << prBoundaries.second << "]" << std::endl;
-    std::cout << "Number of Grid Points 1D: "  << uNumberOfGridPoints1D << std::endl;
-    std::cout << "     Number of Particles: "  << uNumberOfParticles << std::endl;
-    std::cout << "      Grid Point Spacing: "  << dGridPointSpacing << std::endl;
-    std::cout << "    Number of Time Steps: "  << uNumberOfTimeSteps << std::endl;
-    std::cout << "          Time Step Size: "  << dTimeStepSize << std::endl;
-    std::cout << "------------------------------------ MPI ------------------------------------" << std::endl;
-    std::cout << "     Number of Processes: "  << iNumberOfTasks << std::endl;
-    std::cout << "-----------------------------------------------------------------------------" << std::endl;
+    //ds get current rank
+    int iMyRank( 0 );
+
+    //ds get my rank
+    MPI_Comm_rank( MPI_COMM_WORLD, &iMyRank );
+
+    //ds if master
+    if( 0 == iMyRank )
+    {
+        //ds user information
+        std::cout << "\n---------------------------- DIFFUSION PSE SETUP ----------------------------" << std::endl;
+        std::cout << "   Diffusion Coefficient: "  << dDiffusionCoefficient << std::endl;
+        std::cout << "           Boundary (2D): [" << prBoundaries.first << ", " << prBoundaries.second << "]" << std::endl;
+        std::cout << "Number of Grid Points 1D: "  << uNumberOfGridPoints1D << std::endl;
+        std::cout << "     Number of Particles: "  << uNumberOfParticles << std::endl;
+        std::cout << "      Grid Point Spacing: "  << dGridPointSpacing << std::endl;
+        std::cout << "    Number of Time Steps: "  << uNumberOfTimeSteps << std::endl;
+        std::cout << "          Time Step Size: "  << dTimeStepSize << std::endl;
+        std::cout << "------------------------------------ MPI ------------------------------------" << std::endl;
+        std::cout << "     Number of Processes: "  << iNumberOfTasks << std::endl;
+        std::cout << "-----------------------------------------------------------------------------" << std::endl;
+
+        //ds information
+        //std::cout << "               Status:  0% done - current time: 0";
+    }
 
     //ds allocate domain (automatically creates initial density distribution)
     Diffusion::CDomain cDomain( dDiffusionCoefficient, prBoundaries, dBoundarySize, uNumberOfGridPoints1D, uNumberOfParticles, dGridPointSpacing, dTimeStepSize, iNumberOfTasks );
-
-    //ds information
-    //std::cout << "               Status:  0% done - current time: 0";
 
     //ds get the mode mode
     const unsigned int uMode( atoi( argv[3] ) );
@@ -90,63 +100,65 @@ int main( int argc, char** argv )
         //ds start simulation
         for( unsigned int uCurrentTimeStep = 1; uCurrentTimeStep < uNumberOfTimeSteps+1; ++uCurrentTimeStep )
         {
-            //ds calculate percentage done
-            const double dPercentageDone( 100.0*uCurrentTimeStep/uNumberOfTimeSteps );
-
-            //ds and time
-            const double dCurrentTime( uCurrentTimeStep*dTimeStepSize );
-
-            //ds get a formatted string -> 100% -> 3 digits
-            char chBuffer[4];
-
-            //ds fill the buffer
-            std::snprintf( chBuffer, 4, "%3.0f", dPercentageDone );
-
-            //ds print info
-            //std::cout << '\xd';
-            //std::cout << "               Status: " << chBuffer << "% done - current time: " << dCurrentTime;
-
-            //ds my rank
-            int iMyRank( 0 );
-
-            //ds get my rank
-            MPI_Comm_rank( MPI_COMM_WORLD, &iMyRank );
-
             //ds if i am the master
             if( 0 == iMyRank )
             {
+                //ds calculate percentage done
+                const double dPercentageDone( 100.0*uCurrentTimeStep/uNumberOfTimeSteps );
+
+                //ds and time
+                const double dCurrentTime( uCurrentTimeStep*dTimeStepSize );
+
+                //ds get a formatted string -> 100% -> 3 digits
+                char chBuffer[4];
+
+                //ds fill the buffer
+                std::snprintf( chBuffer, 4, "%3.0f", dPercentageDone );
+
+                //ds print info
+                //std::cout << '\xd';
+                //std::cout << "               Status: " << chBuffer << "% done - current time: " << dCurrentTime;
+
                 //ds organize slaves
                 cDomain.updateHeatDistributionNumericalMASTER( );
+
+                //ds streaming
+                //cDomain.saveHeatGridToStream( );
+                cDomain.saveNormsToStream( dCurrentTime );
+                //cDomain.saveMeshToPNG( uCurrentTimeStep, 10 );
             }
             else
             {
                 //ds do the hard work
                 cDomain.updateHeatDistributionNumericalSLAVE( );
             }
-
-            //ds streaming
-            //cDomain.saveHeatGridToStream( );
-            cDomain.saveNormsToStream( dCurrentTime );
-            //cDomain.saveMeshToPNG( uCurrentTimeStep, 10 );
         }
 
-        //ds save the streams to a file
-        //cDomain.writeHeatGridToFile( "bin/simulation.txt", uNumberOfTimeSteps );
-        cDomain.writeNormsToFile( "bin/norms.txt", uNumberOfTimeSteps, dTimeStepSize );
+        //ds if i am the master
+        if( 0 == iMyRank )
+        {
+            //ds save the streams to a file
+            //cDomain.writeHeatGridToFile( "bin/simulation.txt", uNumberOfTimeSteps );
+            cDomain.writeNormsToFile( "bin/norms.txt", uNumberOfTimeSteps, dTimeStepSize );
+        }
     }
 
-    //ds cause an output ostream
-    std::cout << std::endl;
+    //ds if i am the master
+    if( 0 == iMyRank )
+    {
+        //ds cause an output ostream
+        std::cout << std::endl;
+
+        //ds stop timing
+        const double dDurationSeconds( tmTimer.stop( ) );
+
+        //ds done
+        std::cout << "     Computation time: " << dDurationSeconds << std::endl;
+        std::cout << "-----------------------------------------------------------------------------\n" << std::endl;
+    }
 
     //ds shutdown
     MPI_Finalize( );
-
-    //ds stop timing
-    const double dDurationSeconds( tmTimer.stop( ) );
-
-    //ds done
-    std::cout << "     Computation time: " << dDurationSeconds << std::endl;
-    std::cout << "-----------------------------------------------------------------------------\n" << std::endl;
 
     return 0;
 }
